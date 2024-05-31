4d296c5858ba0928fbca55f95244b5e25b04e8e3
Marcos Gutiérrez Alonso <30444886+margual56@users.noreply.github.com>
2024-05-31 13:10:49 +0200

    Add env var support, list flag and license (#1)
    * Add pre-build step for arm devices
    
    * Fix for compiling in ARM; Add list flag and support for env vars
    
    * Add license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Marcos Gutiérrez Alonso
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 mod zone;
 
 use clap::Parser;
@@ -20,15 +44,19 @@ async fn get_ip(client: &reqwest::Client) -> Result<String, reqwest::Error> {
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+    #[arg(short, long, default_value_t = false)]
+    /// Lists all records associated with you account and exits
+    list: bool,
+
     /// List of records (subdomains) to update with your current public IP
     #[arg(short, long)]
     records: Vec<String>,
 
-    #[arg(short, long)]
+    #[arg(short, long, env = "CLOUDFLARE_EMAIL")]
     /// Email for account admin
     email: String,
 
-    #[arg(short, long)]
+    #[arg(short, long, env = "CLOUDFLARE_KEY")]
     /// Global API key
     key: String,
 }
@@ -36,6 +64,22 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
+
+    let client = reqwest::Client::new();
+
+    if args.list {
+        for zone in get_all(&client, &args.email, &args.key).await.unwrap() {
+            for record in zone
+                .get_records(&client, &args.email, &args.key)
+                .await
+                .unwrap()
+            {
+                println!("{}", record.name.yellow());
+            }
+        }
+
+        return Ok(());
+    }
 
     let record_queue = args.records.as_slice();
 
@@ -47,8 +91,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     println!("Set of zones to update: {:?}", zone_queue);
-
-    let client = reqwest::Client::new();
 
     let current_ip = get_ip(&client).await.unwrap();
 
